ce717e81f6c22a9e40e8f838ee15bdf5d59013b3
migueldeoleiros <migueldeoleiros@gmail.com>
2023-02-23 17:09:30 +0000

    validation of commands implemented

--- ticker_client.py
+++ ticker_client.py
@@ -9,18 +9,59 @@ Números de aluno: 58645, 59436
 from net_client import server_connection
 import sys
 
-# Programa principal
-
-# connect to server
+# get parameters
 user = sys.argv[1]
 host = sys.argv[2]
 port = int(sys.argv[3])
 
+
+def validate_run(msg):
+    """Validate the command and run the apropiate function."""
+    # Define a dictionary of valid commands and their number of parameters
+    valid_commands = {
+        'SUBSCR': (2, None),
+        'CANCEL': (1, None),
+        'STATUS': (1, None),
+        'INFOS':  (1, ('M', 'K')),
+        'STATIS': (('L', 2), ('ALL', 1)),
+        'SLEEP':  (1, None),
+        'EXIT':   (0, None)
+    }
+    local_commands = ['SLEEP', 'EXIT']
+
+    # Parse the input string into a command and its parameters
+    parts = msg.split()
+    command = parts[0]
+    parameters = parts[1:]
+
+    # Check if the command is valid and has the right number of parameters
+    if command in valid_commands:
+        param_info = valid_commands[command]
+        if isinstance(param_info[0], str) and parameters[0] == param_info[0]:
+            num_params = param_info[1]
+        elif isinstance(param_info[1], str) and parameters[0] == param_info[1]:
+            num_params = param_info[0]
+        else:
+            num_params = param_info[0]
+
+        if len(parameters) == num_params:
+            if command in local_commands:
+                print('command to execute locally')
+            else:
+                print('command to send server')
+                conn.send_receive(msg.encode('utf-8'))
+        else:
+            print("MISSING-ARGUMENTS")
+    else:
+        print("UNKNOWN-COMMAND")
+
+
+# connect to server
 conn = server_connection(host, port)
 conn.connect()
 
 # send message
 msg = input('>> ')
-conn.send_receive(msg.encode('utf-8'))
+validate_run(msg)
 
 conn.close()
