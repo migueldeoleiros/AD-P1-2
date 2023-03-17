8ae6c4e75fd20572a74d70c987060c59556d68f5
migueldeoleiros <migueldeoleiros@gmail.com>
2023-03-17 17:59:02 +0000

    fix format for coherence

--- ticker_stub.py
+++ ticker_stub.py
@@ -2,38 +2,35 @@ import socket as s
 from net_client import server_connection
 
 class ticker_stub:
-    
 
-    def __init__(self,address,port):
-        self.net_client = server_connection(address,port)
-    
+    def __init__(self, address, port):
+        self.net_client = server_connection(address, port)
+
     def connect(self):
-        self.net_client.connect()    
+        self.net_client.connect()
 
     def disconnect(self):
         self.net_client.close()
 
-    
-    
-    def subscribe(self,IDrec,time,IDcli):
-        self.net_client.send([10,IDrec,time,IDcli])
-        self.net_client.receive()  
-
-    def unsubscribe(self,IDrec,IDcli):
-        self.net_client.send([20,IDrec,IDcli])
+    def subscribe(self, IDrec, time, IDcli):
+        self.net_client.send([10, IDrec, time, IDcli])
         self.net_client.receive()
 
-    def status(self,IDrec,IDcli):
-        self.net_client.send([30,IDrec,IDcli])
+    def unsubscribe(self, IDrec, IDcli):
+        self.net_client.send([20, IDrec, IDcli])
         self.net_client.receive()
 
-    def infos(self,type,IDcli):
-        self.net_client.send([type,IDcli])
+    def status(self, IDrec, IDcli):
+        self.net_client.send([30, IDrec, IDcli])
         self.net_client.receive()
 
-    def statis(self,type,IDrec = None):
-        if IDrec != None:
-            self.net_client.send([type,IDrec])
+    def infos(self, type, IDcli):
+        self.net_client.send([type, IDcli])
+        self.net_client.receive()
+
+    def statis(self, type, IDrec=None):
+        if IDrec is not None:
+            self.net_client.send([type, IDrec])
             self.net_client.receive()
         else:
             self.net_client.send([type])
