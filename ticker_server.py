2b89129fcde1bb0806ab55e89f361dcf4a3dc079
migueldeoleiros <migueldeoleiros@gmail.com>
2023-03-18 14:39:34 +0000

    Add function for sending data and doscstrings

--- ticker_server.py
+++ ticker_server.py
@@ -16,17 +16,15 @@ import struct
 from ticker_pool import resource_pool
 
 
-def process_command(msg, resources, conn_sock):
+def process_command(command, resources, conn_sock):
     """Executa o comando apropriado para uma mensagem.
 
     Args:
-        msg (str): Mensagem recebida do cliente.
+        command (list): Mensagem recebida do cliente.
 
     Returns:
         answer (str): Resposta ao comando executado.
     """
-    command = msg.split()
-
     if command[0] == 'SUBSCR':
         answer = resources.subscribe(int(command[1]), int(command[3]), int(command[2]))
     elif command[0] == 'CANCEL':
@@ -39,16 +37,37 @@ def process_command(msg, resources, conn_sock):
         answer = resources.statis(command[1], int(command[2]))
 
     print(answer)
-    conn_sock.sendall(answer.encode())
+    send_data(answer)
 
 
 def receive_data(sckt):
+    """Recebe dados através do socket especificado.
+
+    Args:
+        sckt: o socket para receber dados.
+    Returns:
+        os dados recebidos.
+    """
     size_bytes = sckt.recv(4)
     size = struct.unpack('i', size_bytes)[0]
 
     msg_bytes = sckt.recv(size)
     msg = pickle.loads(msg_bytes)
     return msg
+
+
+def send_data(sckt, data):
+    """Envia dados através do socket especificado.
+
+    Args:
+        sckt: o socket para enviar dados.
+        data: os dados a serem enviados.
+    """
+    data_bytes = pickle.dumps(data, -1)
+    size_bytes = struct.pack('i', len(data_bytes))
+
+    conn_sock.sendall(size_bytes)
+    conn_sock.sendall(data_bytes)
 
 
 # código do programa principal
